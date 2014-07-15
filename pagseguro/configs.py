dced9e9cc5847a3e7f2a7fb778a31b9f70105fe1
Rafael Nunes <rafael.nunes@gmail.com>
2014-07-15 19:23:57 -0300

    Parametrized Configs for Sandbox

--- pagseguro/configs.py
+++ pagseguro/configs.py
@@ -5,23 +5,20 @@ class Config(object):
     def get(self, key, default=None):
         return getattr(self, key, default)
 
-    BASE_URL = "https://ws.pagseguro.uol.com.br"
-    VERSION = "/v2/"
-    CHECKOUT_SUFFIX = VERSION + "checkout"
-    CHARSET = "UTF-8"  # ISO-8859-1
-    NOTIFICATION_SUFFIX = VERSION + "transactions/notifications/%s"
-    TRANSACTION_SUFFIX = VERSION + "transactions/%s"
-    CHECKOUT_URL = BASE_URL + CHECKOUT_SUFFIX
-    NOTIFICATION_URL = BASE_URL + NOTIFICATION_SUFFIX
-    TRANSACTION_URL = BASE_URL + TRANSACTION_SUFFIX
-    CURRENCY = "BRL"
-    CTYPE = "application/x-www-form-urlencoded; charset={0}".format(CHARSET)
-    HEADERS = {"Content-Type": CTYPE}
-    REFERENCE_PREFIX = "REF%s"
-    PAYMENT_HOST = "https://pagseguro.uol.com.br"
-    PAYMENT_URL = PAYMENT_HOST + CHECKOUT_SUFFIX + "/payment.html?code=%s"
-
-    DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
-    
     def __init__(self, **kwargs):
-      BASE_URL = kwargs['BASE_URL'] or BASE_URL
+      self.BASE_URL =  kwargs.get('BASE_URL', 'https://ws.pagseguro.uol.com.br')
+      self.VERSION = "/v2/"
+      self.CHECKOUT_SUFFIX = self.VERSION + "checkout"
+      self.CHARSET = "UTF-8"  # ISO-8859-1
+      self.NOTIFICATION_SUFFIX = self.VERSION + "transactions/notifications/%s"
+      self.TRANSACTION_SUFFIX = self.VERSION + "transactions/%s"
+      self.CHECKOUT_URL = self.BASE_URL + self.CHECKOUT_SUFFIX
+      self.NOTIFICATION_URL = self.BASE_URL + self.NOTIFICATION_SUFFIX
+      self.TRANSACTION_URL = self.BASE_URL + self.TRANSACTION_SUFFIX
+      self.CURRENCY = "BRL"
+      self.CTYPE = "application/x-www-form-urlencoded; charset={0}".format(self.CHARSET)
+      self.HEADERS = {"Content-Type": self.CTYPE}
+      self.REFERENCE_PREFIX = "REF%s"
+      self.PAYMENT_HOST = "https://pagseguro.uol.com.br"
+      self.PAYMENT_URL = self.PAYMENT_HOST + self.CHECKOUT_SUFFIX + "/payment.html?code=%s"
+      self.DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
